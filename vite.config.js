ae6d991b1e7d46fab90ded9dbd25e16bd19ed927
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-17 10:26:42 +0900

    ビルド環境を調整

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, '../dist'),
+    outDir: path.resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
         embedding_script: path.resolve(__dirname, 'src/embedding_script.ts'),
         content_script: path.resolve(__dirname, 'src/content_script.ts'),
         popup: path.resolve(__dirname, 'src/popup.html'),
-        // 必要に応じて他のエントリーポイントも追加
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
       }
     }
   },
@@ -17,5 +22,15 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
-  }
-})
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+            src: ['src/manifest.json', 'src/*.css', 'src/*.md', 'LICENSE'],
+            dest: '.'
+        }
+      ]
+    })
+  ]
+});
