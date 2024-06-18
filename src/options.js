7d596962e11f7fb92758d845dfac95e509d6ef0e
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-18 11:05:08 +0900

    オプション画面でデリミタの設定を追加

--- src/options.js
+++ src/options.js
@@ -1,15 +1,76 @@
 (() => {
     const CONST = {
         id_fillin_template: "textarea_fillin_template",
+        id_radio_csv_tsv: "radio_csv_tsv",
     };
+
+    const loadOption = (options, key, name) => {
+        key = key == undefined ? null : key;
+        name = name == undefined ? null : name;
+
+        // keyを指定してnameがundefinedの場合は通常のinput要素
+        if (key != null && name == null) {
+            const value = options[key];
+            if (value == undefined) {
+                return "";
+            }
+
+            const elm = document.getElementById(key);
+            if (elm != null) {
+                elm.value = value;
+            }
+        }
+        // keyを指定してnameがnullの場合はradioボタン
+        else if (key == null && name != null) {
+            const value = options[name];
+            if (value == undefined) {
+                return "";
+            }
+
+            const elm = document.getElementsByName(name);
+            if (elm != null) {
+                Array.from(elm).find((e) => e.value == value).checked = true;
+            }
+        }
+    };
+
+    const saveOption = (option, key, name) => {
+        if (option == undefined) {
+            option = {};
+        }
+
+        key = key == undefined ? null : key;
+        name = name == undefined ? null : name;
+
+        // keyを指定してnameがundefinedの場合は通常のinput要素
+        if (key != null && name == null) {
+            const elm = document.getElementById(key);
+            const value = elm.value;
+            option[key] = value;
+            return option;
+        }
+
+        // keyを指定してnameがnullの場合はradioボタン
+        if (key == null && name != null) {
+            const elm = document.getElementsByName(name);
+            option[name] = Array.from(elm).find((e) => e.checked).value;
+            return option;
+        }
+
+        throw new Error("Invalid argument");
+    };
+
     // オプションを読み込む
     document.addEventListener("DOMContentLoaded", function () {
         // 保存された値を読み込む
         chrome.storage.sync.get(null, (options) => {
-            const textarea = document.getElementById(CONST.id_fillin_template);
-            const stored = options[CONST.id_fillin_template];
-            textarea.value = stored == undefined ? "" : stored;
+            // const textarea = document.getElementById(CONST.id_fillin_template);
+            // const stored = options[CONST.id_fillin_template];
+            // textarea.value = stored == undefined ? "" : stored;
             console.log({ options });
+
+            loadOption(options, CONST.id_fillin_template);
+            loadOption(options, null, CONST.id_radio_csv_tsv);
         });
     });
 
@@ -28,13 +89,9 @@
     document
         .getElementById("button_save_options")
         .addEventListener("click", (el) => {
-            const fillin_template = document.getElementById(
-                CONST.id_fillin_template
-            ).value;
-
-            const options = {
-                textarea_fillin_template: fillin_template,
-            };
+            let options = {};
+            options = saveOption(options, CONST.id_fillin_template, null); // idを指定
+            options = saveOption(options, null, CONST.id_radio_csv_tsv); // nameを指定
             chrome.storage.sync.set(options);
             console.log({ options });
         });
