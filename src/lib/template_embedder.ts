3f6781a37bd3fe9c85744c1157d46306251b0652
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-19 16:54:44 +0900

    csv / tsvでレコード抽出した際にヘッダを付与する

--- src/lib/template_embedder.ts
+++ src/lib/template_embedder.ts
@@ -17,21 +17,21 @@ export class TemplateEmbedder {
 
     // kintoneレコードをcsvまたはtsvの形に整形する
     alignment(record: { [key: string]: { value: string } }, mode: 'tsv' | 'csv' = 'csv') {
-        let result = "";
-        if (mode == 'csv') {
-            for (const key in record) {
-                const value = record[key].value;
-                result += value + ",";
-            }
-            result = result.slice(0, -1);
+        const delimtier = mode == 'csv' ? ',' : '\t';
+
+        // レコードのキーをカンマ区切りまたはタブ区切りにする
+        const header = Object.keys(record).join(delimtier);
+
+
+        // レコードの値をカンマ区切りまたはタブ区切りにする
+        let row = [];
+        for (const key in record) {
+            const value = record[key].value;
+            row.push(value);
         }
-        else if (mode == 'tsv') {
-            for (const key in record) {
-                const value = record[key].value;
-                result += value + "\t";
-            }
-            result = result.slice(0, -1);
-        }
+
+        const result = header + '\n' + row.join(delimtier);
+
         return result;
     }
 }
