47c689d542c2cf174fec1888cdc94c33c7eec437
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-21 22:59:08 +0900

    テンプレートに名前を付けることで履歴として保存する

--- src/options.ts
+++ src/options.ts
@@ -2,7 +2,24 @@ import { Utils } from "./utils";
 
 (() => {
     const CONST = Utils.CONST;
+    let templateHistory: { [key: string]: string } = {};
+
     console.log("options.js");
+
+    // input_template_name が空欄でなければ、textarea_fillin_template の値とセットでオプションとして保存する
+    function makeTemplateHistory(): string {
+        const input = document.getElementById(CONST.id_input_template_name) as HTMLInputElement;
+        const templateELement = document.getElementById(CONST.id_fillin_template) as HTMLTextAreaElement;
+
+        if (input && templateELement) {
+            const name = input.value;
+            if (name !== undefined && name !== null && name !== "") {
+                templateHistory[name] = templateELement.value;
+            }
+        }
+
+        return JSON.stringify(templateHistory);
+    }
 
     // オプションを読み込む
     document.addEventListener("DOMContentLoaded", function () {
@@ -12,6 +29,40 @@ import { Utils } from "./utils";
 
             Utils.loadOption(options, Utils.CONST.id_fillin_template, null);
             Utils.loadOption(options, null, Utils.CONST.id_radio_csv_tsv);
+
+            // テンプレート履歴を読み込む
+            const history = options[Utils.CONST.key_template_history];
+            if (history != undefined) {
+                templateHistory = JSON.parse(history);
+                console.log({ templateHistory });
+
+                const select = document.getElementById(CONST.id_select_template_history) as HTMLSelectElement;
+                if (select) {
+                    Object.keys(templateHistory).forEach((key) => {
+                        const option = document.createElement("option");
+                        option.text = key;
+                        option.value = templateHistory[key];
+                        select.add(option);
+                    });
+                }
+            }
+
+            // テンプレート履歴の選択イベント
+            document
+                .getElementById(CONST.id_select_template_history)
+                ?.addEventListener("change", (_el: Event) => {
+                    const select = document.getElementById(CONST.id_select_template_history) as HTMLSelectElement;
+                    const input = document.getElementById(CONST.id_fillin_template) as HTMLTextAreaElement;
+
+                    console.log({ select: select.value })
+                    console.log({ templateHistory })
+
+                    if (select.value !== undefined && select.value !== null) {
+                        const template = select.value;
+                        input.value = template;
+                    }
+                });
+
         });
     });
 
@@ -21,7 +72,14 @@ import { Utils } from "./utils";
             let options: { [key: string]: string } = {};
             options = Utils.saveOption(options, CONST.id_fillin_template, null); // idを指定
             options = Utils.saveOption(options, null, CONST.id_radio_csv_tsv); // nameを指定
+
+            const history = makeTemplateHistory()
+            console.log({ history })
+            options[Utils.CONST.key_template_history] = history;
+
             chrome.storage.sync.set(options);
             console.log({ options });
         });
+
+
 })();
