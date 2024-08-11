c716dcab9eef15235672bd4ad5a61a1304f4681e
Daisuke Motohashi <ddssk.m@gmail.com>
2024-08-11 22:38:32 +0900

    サブテーブルを配列としてテンプレートに展開する機能を実装

--- src/lib/template_embedder.ts
+++ src/lib/template_embedder.ts
@@ -1,4 +1,7 @@
 // kintoneのフィールドコードをプレースホルダとして埋め込んだテンプレートにデータを埋め込む
+
+export type Record = { [key: string]: { value: string } };
+
 export class TemplateEmbedder {
     template: string;
 
@@ -6,7 +9,7 @@ export class TemplateEmbedder {
         this.template = template;
     }
 
-    embedRecords(records: { [key: string]: { value: string } }[], template: string | undefined = undefined) {
+    embedRecords(records: Record[], template: string | undefined = undefined) {
         if (template == undefined) {
             template = this.template;
         }
@@ -20,7 +23,7 @@ export class TemplateEmbedder {
         return embeds;
     }
 
-    embed(record: { [key: string]: { value: string } }, template: string | undefined = undefined) {
+    embed(record: Record, template: string | undefined = undefined) {
         if (template == undefined) {
             template = this.template;
         }
@@ -29,17 +32,57 @@ export class TemplateEmbedder {
         let filledTemplate = template;
         for (const key in record) {
             let value = record[key].value;
+
+            // valueが配列の場合、%PLACEHOLDER{ で始まり、}%でおわる複数行のテンプレートに対応する
+            if (Array.isArray(value)) {
+                filledTemplate = this.fillArrayTemplate(filledTemplate, key, value);
+            }
             // valueが改行を含む場合は\nにエスケープする
-            if (typeof value === 'string') {
+            else if (typeof value === 'string') {
                 value = value.replace(/\n/g, '\\n');
+
+                // 通常は %PLACEHOLDER% という改行を含まないプレースホルダを置換する
+                filledTemplate = filledTemplate.replace(`%${key}%`, value);
             }
 
-            console.log({ key, value })
-            filledTemplate = filledTemplate.replace(`%${key}%`, value);
+            // console.log({ key, value })
+
         }
 
         console.log({ filledTemplate })
         return filledTemplate;
+    }
+
+    // %PLACEHOLDER{ で始まり、}%でおわる複数行のテンプレートに対応する
+    fillArrayTemplate(template: string, key: string, subRows: { id: string, value: Record }[]) {
+        console.log("--- fillArrayTemplate")
+        const start = `%${key}{`;
+        const end = `}%`;
+
+        const startIndex = template.indexOf(start);
+        const endIndex = template.indexOf(end, startIndex);
+
+        if (startIndex == -1 || endIndex == -1) {
+            return template;
+        }
+
+        const prefix = template.slice(0, startIndex);
+        const suffix = template.slice(endIndex + end.length);
+        const subtable_placeholder = template.slice(startIndex + start.length, endIndex);
+
+        console.log({ subRows: subRows })
+        const filledTemplate = subRows.map((subRow) => {
+            console.log({ subRow })
+            console.log({ value: subRow.value })
+            const keys = Object.keys(subRow.value);
+            return keys.reduce((table_template, key: string) => {
+                console.log({ key })
+                const cell_text = subRow.value[key].value
+                return table_template.replace(`%${key}%`, cell_text)
+            }, subtable_placeholder)
+        }).join('');
+
+        return prefix + filledTemplate + suffix;
     }
 
     // kintoneレコードをcsvまたはtsvの形に整形する
