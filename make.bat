4f8de759a671651f9721ba4353848ed210415030
Daisuke Motohashi <ddssk.m@gmail.com>
2024-09-16 06:45:41 +0900

    バッチファイルにコメントを追加

--- make.bat
+++ make.bat
@@ -5,12 +5,16 @@ REM distディレクトリを丸ごとzip化するコマンド
 set "dist_dir=dist"
 set "zip_file=dist.zip"
 
-REM 既存のzipファイルがあれば削除
+echo 既存のzipファイルがあれば削除します
 if exist "%zip_file%" del "%zip_file%"
 
-REM distディレクトリをzip化
+echo productionビルドを実行します。
+call npm run build
+
+echo distディレクトリをzip化します
 powershell Compress-Archive -Path "%dist_dir%\*" -DestinationPath "%zip_file%"
 
 echo distディレクトリを%zip_file%に圧縮しました。
 
+echo %zip_file% をアップロードしてください。
 endlocal
