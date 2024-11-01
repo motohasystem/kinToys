9d8b20a7a4b6c6deb17e9b6bcb7ccb374fbdfc76
Daisuke Motohashi <ddssk.m@gmail.com>
2024-11-01 16:37:50 +0900

    コンテキストメニューにシンプルURLのコピーを追加

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,53 @@
 // import { TablePicker } from "./lib/table_picker";
 
+
 (() => {
+    const CONTEXT_MENU = {
+        copy_simple_url: "copySimpleUrl"
+    }
+
+
     let connection: chrome.runtime.Port | null = null;
 
     chrome.runtime.onInstalled.addListener(() => {
         console.log('拡張がインストールされました');
+
+        // 
+        chrome.contextMenus.create({
+            id: "copySimpleUrl",
+            title: "Copy Simple URL",
+            contexts: ["action"]
+        });
+
+        chrome.contextMenus.onClicked.addListener((info, tab) => {
+            console.log(`contextMenus.onClicked: ${info}`);
+            if (info.menuItemId === CONTEXT_MENU.copy_simple_url) {
+                // シンプルURLのコピー
+                if (tab != undefined && tab.id != undefined) {
+                    // ブラウザのアドレスバーのURLをクリップボードにコピーする
+                    chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        func: () => {
+                            let simplified = window.location.href;
+
+                            // 1つ目のURL: 先頭からrecord=43までを残す
+                            simplified = simplified.replace(/(#record=\d+).*/, '$1');
+
+                            // 2つ目のURL: ?view=\d までを残す。q&が入る場合を考慮する。
+                            simplified = simplified.replace(/(\?q?&?view=\d+).*/, '$1');
+
+                            navigator.clipboard.writeText(simplified).then(() => {
+                                console.log('URLをクリップボードにコピーしました');
+                            }).catch(err => {
+                                console.error('クリップボードへのコピーに失敗しました:', err);
+                            });
+                        }
+                    });
+                }
+            }
+        });
     });
+
 
     // 接続成立時
     chrome.runtime.onConnect.addListener((port) => {
