d43ab1fd367ece78533b1eb24348cc4d19508774
Daisuke Motohashi <ddssk.m@gmail.com>
2024-12-21 20:46:28 +0900

    コンテキストメニューのリスナー登録タイミングを修正

--- src/background.ts
+++ src/background.ts
@@ -28,63 +28,58 @@ import { Utils } from "./utils";
             title: "Toggle Newline",
             contexts: ["action"]
         });
+    });
 
-        chrome.contextMenus.onClicked.addListener((info, tab) => {
-            console.log(`contextMenus.onClicked: ${info}`);
-            if (info.menuItemId === CONTEXT_MENU.copy_simple_url) {
-                // シンプルURLのコピー
-                if (tab != undefined && tab.id != undefined) {
-                    // ブラウザのアドレスバーのURLをクリップボードにコピーする
-                    chrome.scripting.executeScript({
-                        target: { tabId: tab.id },
-                        func: () => {
-                            let simplified = window.location.href;
 
-                            // 1つ目のURL: 先頭からrecord=43までを残す
-                            simplified = simplified.replace(/(#record=\d+).*/, '$1');
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        console.log(`contextMenus.onClicked: ${info}`);
+        if (info.menuItemId === CONTEXT_MENU.copy_simple_url) {
+            // シンプルURLのコピー
+            if (tab != undefined && tab.id != undefined) {
+                // ブラウザのアドレスバーのURLをクリップボードにコピーする
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    func: () => {
+                        let simplified = window.location.href;
 
-                            // 2つ目のURL: ?view=\d までを残す。q&が入る場合を考慮する。
-                            simplified = simplified.replace(/\?q?&?(view=\d+).*/, '?$1');
+                        // 1つ目のURL: 先頭からrecord=43までを残す
+                        simplified = simplified.replace(/(#record=\d+).*/, '$1');
 
-                            window.focus();
-                            navigator.clipboard.writeText(simplified).then(() => {
-                                console.log('URLをクリップボードにコピーしました');
-                            }).catch(err => {
-                                console.error('クリップボードへのコピーに失敗しました:', err);
-                            });
+                        // 2つ目のURL: ?view=\d までを残す。q&が入る場合を考慮する。
+                        simplified = simplified.replace(/\?q?&?(view=\d+).*/, '?$1');
 
+                        window.focus();
+                        navigator.clipboard.writeText(simplified).then(() => {
+                            console.log('URLをクリップボードにコピーしました');
+                        }).catch(err => {
+                            console.error('クリップボードへのコピーに失敗しました:', err);
+                        });
+
+                    }
+                });
+            }
+            else {
+                console.error('タブ情報が取得できませんでした');
+            }
+        }
+        else if (info.menuItemId === CONTEXT_MENU.toggle_newline) {
+            console.log('Toggle BreakLineOption');
+
+            // メッセージを送る
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                if (tabs.length > 0) {
+                    chrome.tabs.sendMessage(tabs[0].id as number, { name: Utils.Messages.changeBreaklineOption }, (_response) => {
+                        console.log('toggle_newline message sent.');
+                        if (chrome.runtime.lastError) {
+                            console.error('Error sending message:', chrome.runtime.lastError);
+                            return;
                         }
                     });
                 }
-                else {
-                    console.error('タブ情報が取得できませんでした');
-                }
-            }
-            else if (info.menuItemId === CONTEXT_MENU.toggle_newline) {
-                console.log('Toggle BreakLineOption');
+            });
 
-                // メッセージを送る
-                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                    if (tabs.length > 0) {
-                        chrome.tabs.sendMessage(tabs[0].id as number, { name: Utils.Messages.changeBreaklineOption }, (_response) => {
-                            console.log('toggle_newline message sent.');
-                            if (chrome.runtime.lastError) {
-                                console.error('Error sending message:', chrome.runtime.lastError);
-                                return;
-                            }
-                        });
-                    }
-                });
-
-            }
-        });
-
+        }
     });
-
-    // chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, _tab) => {
-
-    // });
-
 
     // 接続成立時
     chrome.runtime.onConnect.addListener((port) => {
